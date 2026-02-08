2c436d573ff1584e76f643737a50ac282786f597
Eduardo Villagomez <eddvillagomez1@gmail.com>
2026-02-07 20:17:44 -0500

    Create data.py

---
+++ data.py
@@ -0,0 +1,9 @@
+# File with constants
+URBAN_ROUTES_URL = 'https://cnt-4cb52b12-8a05-48c0-8065-e07a1d08a757.containerhub.tripleten-services.com'
+
+ADDRESS_FROM = 'East 2nd Street, 601'
+ADDRESS_TO = '1300 1st St'
+PHONE_NUMBER = '+1 123 123 12 12'
+CARD_NUMBER = '1234 5678 9100'
+CARD_CODE = '1111'
+MESSAGE_FOR_DRIVER = 'Stop at the juice bar, please'
